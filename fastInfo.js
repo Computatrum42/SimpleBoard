f9961607749f0fd06f6857d66e227c36abcaf4ce
Computatrum42 <ergunsarigul29a@outlook.com>
2024-08-02 00:40:52 +0300

    commit test commit test thats it

---
+++ fastInfo.js
@@ -0,0 +1,8 @@
+// Write your age, gender, nation, etc. You can expand the facts by making a variable and adding into document write thing.
+var age = "97"
+var gender = "Probably male"
+var nation = "Hell"
+var occupation = "Nissan GT-R Black Edition"
+
+document.write
+("<h2>Fast Facts</h2> My Age: " + age + "<br> Gender: " + gender + "<br> Where am I from: " + nation + "<br> I'm a: " + occupation)
